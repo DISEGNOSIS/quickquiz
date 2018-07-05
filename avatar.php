1d3c5278137f5c4d6878b52371cedfe7f3047acc
MarielaFloridia <mariela.floridia@gmail.com>
2018-07-05 18:22:17 -0300

    Mejorado... graba
    graba la imagen, pero falta que levante

--- avatar.php
+++ avatar.php
@@ -10,26 +10,28 @@
 	public $nomDir; 
 	public $nomAvatar; 
 	public $archivoFinal; 
-
+	public $locFinal;
 
  	function __construct()	{
  		$original = $_FILES["avatar"];
  		if($original["error"] === UPLOAD_ERR_OK) {
-			$this->$nombreViejo = $original["name"];
-			$this->$extension = pathinfo($nombreViejo, PATHINFO_EXTENSION);
-			$this->$nombreNuevo = $original["tmp_name"];
-			$this->$archivoFinal = dirname(__FILE__);
-			$this->$nomDir = "/avatar/";
-			$this->$nomAvatar =  uniqid() . "." . $extension;
-			/*var_dump($nombreNuevo, $archivoFinal);exit;*/
-			$this->$archivoFinal = $nomDir . $nomAvatar;
-			move_uploaded_file($this->$nombreNuevo, $this->$archivoFinal);
+			$this->nombreViejo = $original["name"];
+			$this->extension = pathinfo($this->nombreViejo, PATHINFO_EXTENSION);
+			$this->nombreNuevo = $original["tmp_name"];
+			$this->archivoFinal = realpath(dirname(__FILE__) . '/../');
+			$this->nomDir = "/avatar/";
+			$this->nomAvatar =  uniqid() . "." . $this->extension;
+			$this->locFinal = $this->archivoFinal .$this->nomDir . $this->nomAvatar;
+			$this->archivoFinal = $this->nomDir . $this->nomAvatar;
+			var_dump($this->nombreNuevo, 'archivoFinal: ' . $this->archivoFinal);
+			//exit;
+			move_uploaded_file($this->nombreNuevo, $this->locFinal);
 		}
  		# code...
  	}
 
  	function getNombre(){
- 		return $this->$nomAvatar; 
+ 		return $this->$archivoFinal; 
  	}
  } 
 ?>
